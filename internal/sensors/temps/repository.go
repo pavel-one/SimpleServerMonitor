9c651e03e1062a24eba92e83e4bcca29e204c015
Pavel Zarubin <paha996@yandex.ru>
2023-07-24 17:23:35 +0300

    Dev sql repository

--- internal/sensors/temps/repository.go
+++ internal/sensors/temps/repository.go
@@ -1,19 +1,37 @@
 package temps
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/pavel-one/SimpleServerMonitor/internal/db"
 )
 
-type Repository struct {
+var schema = `
+CREATE TABLE IF NOT EXISTS Temps
+(
+    id		VARCHAR NOT NULL PRIMARY KEY,
+    temp 	INTEGER NOT NULL,
+    time 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`
+
+type StatRepository struct {
 	DB *sqlx.DB
 }
 
-func NewRepository() (*Repository, error) {
+func NewStatRepository() (*StatRepository, error) {
 	connection, err := db.DefaultConnection()
 	if err != nil {
 		return nil, err
 	}
 
-	return &Repository{DB: connection}, nil
+	if _, err := connection.Exec(schema); err != nil {
+		return nil, fmt.Errorf("error create schema: %s", err)
+	}
+
+	return &StatRepository{DB: connection}, nil
+}
+
+func (r *StatRepository) Save(stat *Stat) error {
+
 }
